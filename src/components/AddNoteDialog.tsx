2097e9d56fedd95c249f6fcc56405aa87080dcfd
Elias Shemsu <echosierran@gmail.com>
2024-07-23 10:47:17 +0300

    Loading Button component

--- src/components/AddNoteDialog.tsx
+++ src/components/AddNoteDialog.tsx
@@ -3,7 +3,16 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Dialog, DialogContent, DialogHeader } from "./ui/dialog";
 import { DialogTitle } from "@radix-ui/react-dialog";
-import { Form } from "./ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 interface AddNoteDialogProps {
   open: boolean;
@@ -26,10 +35,34 @@ export default function AddNoteDialog({ open, setOpen }: AddNoteDialogProps) {
           <DialogTitle>Add Note</DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-3"
-          ></form>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Note title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note Content</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Note content" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
         </Form>
       </DialogContent>
     </Dialog>
