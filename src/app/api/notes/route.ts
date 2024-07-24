224bbb77e2c50c70a91740329331b0ca0a2cd0a5
Elias Shemsu <echosierran@gmail.com>
2024-07-24 10:47:54 +0300

    note update route

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,5 +1,5 @@
 import prisma from "@/lib/db/prisma"
-import { createNoteSchema } from "@/lib/validation/note"
+import { createNoteSchema, updateNoteSchema } from "@/lib/validation/note"
 import { auth } from "@clerk/nextjs"
 
 export async function POST(request: Request) {
@@ -28,6 +28,44 @@ export async function POST(request: Request) {
         })
 
         return Response.json({ note }, { status: 201 })
+
+    } catch (error) {
+        console.log(error)
+        return Response.json({ error: "Internal server error" }, { status: 500 })
+    }
+}
+
+export async function PUT(request: Request) {
+    try {
+        const body = await request.json();
+        const parseResult = updateNoteSchema.safeParse(body);
+
+        if (!parseResult.success) {
+            console.log(parseResult.error)
+            return Response.json({ error: "Invalid input" }, { status: 400 })
+        }
+
+        const { id, title, content } = parseResult.data
+
+        const note = await prisma.note.findUnique({ where: { id } })
+        if (!note) {
+            return Response.json({ error: "Note not found" }, { status: 400 })
+        }
+
+        const { userId } = auth()
+
+        if (!userId || userId !== note.userId) {
+            return Response.json({ error: "unauthorized", status: 401 })
+        }
+
+        const updatedNote = await prisma.note.update({
+            where: { id },
+            data: {
+                title, content
+            }
+        })
+
+        return Response.json({ note: updatedNote }, { status: 200 })
 
     } catch (error) {
         console.log(error)
